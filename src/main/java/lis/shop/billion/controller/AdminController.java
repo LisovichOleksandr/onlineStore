3825627de878ae9d7e66425ad7e4df50c6ed26a2
LisovichOleksandr <lisovyhs@gmail.com>
2025-03-15 08:37:37 +0200

    refactor of AdminController added comments

--- src/main/java/lis/shop/billion/controller/AdminController.java
+++ src/main/java/lis/shop/billion/controller/AdminController.java
@@ -5,10 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Контролер для управління адміністративними операціями.
+ * Цей контролер обробляє HTTP-запити, призначені для адміністраторів.
+ */
+
 @Controller
 @RequestMapping("/api/admin")
 public class AdminController {
 
+    /**
+     * Обробляє GET-запити за шляхом "/api/admin".
+     *
+     * @return ResponseEntity зі статусом 200 OK і повідомленням "You are ADMIN!!!!!"
+     */
     @GetMapping
     public ResponseEntity<String> getMapping(){
         return ResponseEntity.ok().body("You are ADMIN!!!!!");
